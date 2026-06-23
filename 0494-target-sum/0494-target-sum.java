a9c93d924c0401088866d1b5bae0cab9eeecb264
Adarsh Gupta <144550701+Adarsh013@users.noreply.github.com>
2026-06-23 20:16:33 +0530

    Time: 86 ms (41.61%), Space: 47.5 MB (6.18%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,34 +1,31 @@
 class Solution {
 
+    // Since target can be negaitve number we cant use normal dp Array
+    Map<String, Integer> dp = new HashMap<>();
+
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums.length - 1, target, nums);
+        int n = nums.length;
+        return helper(n - 1, target, nums);
     }
 
     int helper(int idx, int target, int[] nums) {
 
         if(idx == 0) {
-
             int count = 0;
-
-            if(target == nums[0])
-                count++;
-
-            if(target == -nums[0])
-                count++;
+            if(target == nums[0]) count++;
+            if(target == -nums[0]) count++;
 
             return count;
         }
 
-        int positive =
-            helper(idx - 1,
-                   target - nums[idx],
-                   nums);
+        String key = idx + "," + target;
+        if(dp.containsKey(key)) return dp.get(key);
 
-        int negative =
-            helper(idx - 1,
-                   target + nums[idx],
-                   nums);
-
-        return positive + negative;
+        int positive = helper(idx - 1,target - nums[idx], nums);
+        int negative = helper(idx - 1,target + nums[idx], nums);
+        
+        int ans = positive + negative;
+        dp.put(key, ans) ;
+        return ans;
     }
 }
