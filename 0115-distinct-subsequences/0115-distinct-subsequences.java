a05d8cce61334a50f4010bf669e38dd6b41071c3
Adarsh Gupta <144550701+Adarsh013@users.noreply.github.com>
2026-06-26 12:54:30 +0530

    Time: 19 ms (72.51%), Space: 54.5 MB (30.58%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,27 +1,23 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int i = s.length();
-        int j = t.length();
+        int n = s.length();
+        int m = t.length();
 
-        int [][] dp = new int[i+1][j+1];
-        for(int val[] : dp){
-            Arrays.fill(val, -1);
+        int [][] dp = new int[n+1][m+1];
+
+        for(int i = 0; i<=n; i++) dp[i][0] = 1;
+        for(int j = 1; j<=m; j++) dp[0][j] = 0;
+
+        for(int i = 1; i<=n; i++) {
+            for(int j = 1; j<=m; j++){
+
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j-1] + dp[i - 1][j];
+                }
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
         }
-
-        return helper(i-1, j-1, s, t, dp);
-    }
-
-    private int helper(int i, int j, String s, String t, int[][] dp){
-        // Base cases
-
-        if(j < 0) return 1; // if all the character of string t got matched
-        if(i < 0) return 0; // if there's still remaining character and i went to -ve idx
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = helper(i - 1, j-1, s, t, dp) + helper(i - 1, j, s, t, dp);
-        }
-        return dp[i][j] = helper(i - 1, j, s, t, dp);
+        return dp[n][m];
     }
 }
