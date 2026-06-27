cec50a76c9c423494bbf15ff4eb33d4163219035
Adarsh Gupta <144550701+Adarsh013@users.noreply.github.com>
2026-06-27 15:10:40 +0530

    Time: 6 ms (49.78%), Space: 76 MB (58.25%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -2,24 +2,24 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
 
-        int[][][] dp = new int[n+1][2][3];
+        int[][] after = new int[2][3];
+        int[][] curr = new int[2][3];
 
         for(int idx = n-1; idx >=0; idx--){
             for (int buy = 0; buy<=1; buy++){
                 for(int cap = 1; cap<=2; cap++){
                     if(buy == 1) {
 
-                        dp[idx][buy][cap]  = Math.max(-prices[idx] + dp[idx + 1][0][cap],
-                                                         dp[idx + 1][1][cap]);
+                       curr[buy][cap]  = Math.max(-prices[idx] + after[0][cap],after[1][cap]);
 
                     } else {
 
-                        dp[idx][buy][cap]  = Math.max(prices[idx] + dp[idx + 1][1][cap - 1],
-                                                     dp[idx + 1][0][cap]);
+                       curr[buy][cap]  = Math.max(prices[idx] + after[1][cap - 1], after[0][cap]);
                     }
                 }
             }
+            after = curr;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 }
