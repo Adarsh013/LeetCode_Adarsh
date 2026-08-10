e0cddc4da2bba351910e877cb9629f07e27f9479
Adarsh Gupta <144550701+Adarsh013@users.noreply.github.com>
2026-08-11 00:21:17 +0530

    Time: 2 ms (54.35%), Space: 43.5 MB (41.88%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         if(s.length() != goal.length()) return false;
-
-        return (s+s).contains(goal);
+        
+        String doubledS = s + s;
+        // Check if the goal is a substring of the concatenated string
+        return doubledS.contains(goal);
     }
 }
