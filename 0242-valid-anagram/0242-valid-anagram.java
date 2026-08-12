7461e3e0002ccb4760ad7f44cac9165ccaa05d68
Adarsh Gupta <144550701+Adarsh013@users.noreply.github.com>
2026-08-12 10:39:46 +0530

    Time: 5 ms (61.73%), Space: 44.3 MB (75.01%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[128];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a' ]--;
+            // count[s.charAt(i)]++;
+            // count[t.charAt(i)]--;
+        }
+
+        for (int x : count) {
+            if (x != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
